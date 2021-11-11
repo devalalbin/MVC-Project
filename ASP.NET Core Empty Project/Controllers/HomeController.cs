a85c4600dfb8f081bcf57c78de25478a095caf40
devalalbin <vala@student.chalmers.se>
2021-11-11 17:01:42 +0100

    Add project files.

---
+++ ASP.NET Core Empty Project/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Empty_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.test = "This is a test";
+            return View();
+        }
+    }
+}
