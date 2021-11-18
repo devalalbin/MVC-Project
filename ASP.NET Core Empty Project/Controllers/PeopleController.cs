0792df7aa2a182180cba9a295afbc0acd72a1fd1
devalalbin <vala@student.chalmers.se>
2021-11-18 23:27:27 +0100

    new branch assignement 1

---
+++ ASP.NET Core Empty Project/Controllers/PeopleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Empty_Project.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult People()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET Core Empty Project/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Empty_Project.Models
+{
+    public class Person
+    {
+        // name phone number and a city
+    }
+}
