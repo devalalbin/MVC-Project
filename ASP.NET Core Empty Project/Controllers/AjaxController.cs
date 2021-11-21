191f1542c5e973a4c5c10b8e1ff90cd72ee5e27c
devalalbin <vala@student.chalmers.se>
2021-11-22 00:51:19 +0100

    working ajax deletion

--- ASP.NET Core Empty Project/Controllers/AjaxController.cs
+++ ASP.NET Core Empty Project/Controllers/AjaxController.cs
@@ -16,7 +16,7 @@ namespace ASP.NET_Core_Empty_Project.Controllers
 
             PeopleViewModel vm = new PeopleViewModel() { PeopleListView = pu.Read() }; //returns the list of persons we created
 
-            if (vm.PeopleListView.Count == 0 || vm.PeopleListView == null) // Fills the list with 3 names if emptu (If you delete all people in list it will fill again)
+            if (vm.PeopleListView.Count == 0 || vm.PeopleListView == null) // Fills the list with 3 names if empty
             {
                 pu.GeneratePeople();
             }
@@ -36,12 +36,30 @@ namespace ASP.NET_Core_Empty_Project.Controllers
         {
             PersonUtility pu = new PersonUtility();
             Person targetPerson = pu.Read(personId);
-            List<Person> people = new List<Person>();
+            List<Person> people = new List<Person>(); //making a list of 1 person, since our partialview takes list of persons
             if (targetPerson != null)
             {
                 people.Add(targetPerson);
             }
             return PartialView("_partialPerson", people);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePerson(int personId)
+        {
+            PersonUtility pu = new PersonUtility();
+            Person targetPerson = pu.Read(personId);
+            Console.WriteLine("this is the ID we wanna delete" + targetPerson);
+            bool success = false;
+            if(targetPerson != null)
+            {
+                success = pu.Delete(targetPerson);
+            }
+            if (success) // if deletion did not work, return error codes
+            {
+                return StatusCode(200);
+            }
+            return StatusCode(404);
         }
 
     }
--- ASP.NET Core Empty Project/wwwroot/js/PersonScript.js
+++ ASP.NET Core Empty Project/wwwroot/js/PersonScript.js
@@ -1,5 +1,5 @@
 ﻿function getPeople() {
-    //console.log("Kom vi in hit?");
+    console.log("Did we make it in the function by clicking?");
     $.get("/Ajax/GetPeople", null, function (data) {
         $("#PeopleList").html(data);
     });
@@ -8,8 +8,26 @@
 
 function getPersonById() {
     var personIdValue = document.getElementById('PersonId').value;
-    console.log(personIdValue+" this is our value");
+    console.log(personIdValue+" Test person id value");
     $.post("/Ajax/Search", { personId: personIdValue }, function (data) {
         $("#PeopleList").html(data);
     });
+}
+
+
+
+function deletePerson() {
+    var personIdValue = document.getElementById('PersonId').value;
+    $.post("/Ajax/DeletePerson", { personId: personIdValue }, function (data) {
+
+    })
+        .done(function () {
+            console.log(personIdValue + " deleted this person with ID" );
+            document.getElementById('errorMessages').innerHTML = "Deleted person.";
+            getPeople();
+        })
+        .fail(function () {
+            document.getElementById('errorMessages').innerHTML = "Something went wrong, could not delete the person";
+        });
+
 }
